a35af0645bd3fa0f606a1a65f8f129efafcb2504
bhawanaprasain <bhawana.prs@gmail.com>
2021-02-09 13:23:22 +0545

    socket connnection

--- client/src/Pages/Message/Message.js
+++ client/src/Pages/Message/Message.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React ,{useEffect} from 'react'
 import { useHistory } from "react-router-dom";
 import Toolbar from '../../Dashboard/Navbar/Toolbar/Toolbar'
 import Aux from '../../hoc/Auxilliary'
+import io from 'socket.io-client'
 import './Message.css'
 import tomato from '../../Assets/Images/tomato.jpeg'
-
+var socket;
+const ENDPOINT = "http://localhost:4000/"
 const Message=()=>{
+
+    useEffect (()=>{
+        // const {name,room,id} = queryString.parse(location.search)
+        socket = io(ENDPOINT)
+        socket.emit("connection",()=>{})
+        
+        return()=>{
+            socket.emit("disconnect");
+            socket.off()
+        }
+         
+    }, [])
+
+
     let history=useHistory()
     const handleClick=()=>{
       history.push("/message/userid")
